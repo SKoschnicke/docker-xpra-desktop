300a1bcb2b8ca5a1a87e87cde3bc4010c722f0ef
Sven Koschnicke <s.koschnicke@gfxpro.com>
2015-04-23 13:52:22 +0200

    updated dockerfile, added separate start script

---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+echo "you can connect to the graphical seesion using:"
+echo "> xpra attach tcp:localhost:10000"
+echo "if the container doesn't run on you local machine, you can tunnel the connection:"
+echo "> xpra attach hostname:10000"
+
+xpra start :100 --no-daemon --start-child=xterm --bind-tcp=0.0.0.0:10000
